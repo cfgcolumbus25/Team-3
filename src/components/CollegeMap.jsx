603bbec10aeb3a5d75d1cd060361ffec4d40aaca
Nithin Seelan <nithindseelan@gmail.com>
2025-11-08 01:05:19 -0500

    Implemented maps feature in learners' view

--- src/components/CollegeMap.jsx
+++ src/components/CollegeMap.jsx
@@ -1,11 +1,31 @@
+import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import collegesData from "../data/university_data_geocoded.json";
 
+// Fix for default marker icons in react-leaflet
+delete (L.Icon.Default.prototype)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+});
+
 const CollegeMap = () => {
+  useEffect(() => {
+    // Ensure map tiles load properly
+    return () => {};
+  }, []);
+
   return (
-    <div style={{ height: "80vh", width: "100%" }}>
-      <MapContainer center={[39.8283, -98.5795]} zoom={4}>
+    <div style={{ height: "100%", width: "100%" }}>
+      <MapContainer 
+        center={[39.8283, -98.5795]} 
+        zoom={4}
+        style={{ height: "100%", width: "100%" }}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
